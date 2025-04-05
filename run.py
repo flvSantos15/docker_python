19edaecc02bfb0985b624c65488de74a669f5fd4
flvsantos15 <flvsantos300@gmail.com>
2025-04-05 01:30:57 -0300

    chore: add users route

--- run.py
+++ run.py
@@ -1,7 +1,19 @@
-from flask import Flask
+from flask import Flask, request
+
+from src import UserRepository
 
 app = Flask(__name__)
 
 @app.route("/", methods=["GET"])
 def hello_world():
   return "<p>Hello, World!</p>"
+
+@app.route("/insert", methods=["POST"])
+def insert_user():
+  user_repository = UserRepository()
+  body = request.get_json()
+  
+  name = body["name"]
+  user_repository.insert_user(name)
+  
+  return "User inserted"
